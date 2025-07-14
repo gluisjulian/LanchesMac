270606e26e7e9bfe8ea9ad7bf4e3cbe0e164e6f1
Gabriel Julian <gluisjulian@gmail.com>
2025-07-14 18:15:19 -0300

    Criação do AppDbContext

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,7 +1,17 @@
+using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+                            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
 
 var app = builder.Build();
 
