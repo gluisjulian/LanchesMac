197be6c9acd7978b186c41d021a378e074911ca6
Gabriel Julian <gluisjulian@gmail.com>
2025-07-17 21:16:30 -0300

    Adicionado Injeção de dependencia dos repositorios. Criado os repositorios Lanche e Categoria.

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,4 +1,6 @@
 using LanchesMac.Context;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => 
                             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
